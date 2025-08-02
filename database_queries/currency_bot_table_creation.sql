1e8d96b659f7fc0d9e23fc1f091d7c2c428db99b
alenati <alanabluecake@gmail.com>
2025-08-02 13:05:43 +0200

    fix: currency_num is a Primary Key now

--- database_queries/currency_bot_table_creation.sql
+++ database_queries/currency_bot_table_creation.sql
@@ -3,24 +3,23 @@ drop table if exists currency_cost cascade;
 drop table if exists user_choice cascade;
 
 create table currency (
-	currency_id serial primary key,
 	currency_code varchar(3) not null unique,
-	currency_num varchar(3) not null unique,
+	currency_num varchar(3) primary key,
 	currency_name varchar(50) not null
 );
 
 create table user_choice (
 	user_id bigint not null,
-	currency_id integer not null references currency(currency_id),
+	currency_num varchar(3) not null references currency(currency_num),
 	created_at timestamp default current_timestamp,
-	primary key (user_id, currency_id)
+	primary key (user_id, currency_num)
 );
 
 create table currency_cost (
 	id serial primary key,
 	date timestamp not null default current_timestamp,
-	currency_id integer not null references currency(currency_id),
+	currency_num varchar(3) not null references currency(currency_num),
 	rate decimal(10,4) not null,
 	unit integer not null,
-	unique (date, currency_id)
+	unique (date, currency_num)
 )
