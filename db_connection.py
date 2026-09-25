2f9369e26f632af1f66e768be4fa84672856f58c
Alena Tikhoblazhenko <alanabluecake@gmail.com>
2026-09-25 13:56:37 +0200

    feat: logging added to pg db connection

--- db_connection.py
+++ db_connection.py
@@ -1,10 +1,14 @@
 import asyncpg
 from dotenv import load_dotenv
 import os 
+import logging
+# from logging_config import setup_logger
 
 #to run: await connect()
 
 async def connect():
+    # setup_logger()
+    # logger = logging.getLogger(__name__)
     load_dotenv()
     connection = await asyncpg.connect(
                 host = os.getenv("PG_HOST"),
@@ -12,4 +16,11 @@ async def connect():
                 password = os.getenv("PG_PASSWORD"),
                 database = os.getenv("PG_DB_NAME")
             )
-    return connection
+    if connection: 
+        logging.info("[POSTGRES AUTH] Successful")
+        return connection
+    else:
+        logging.error("[POSTGRES AUTH] Failed")
+    
+# if __name__ == '__main__':
+#     asyncio.run(connect())
