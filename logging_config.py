d16cc11aec8053e696601313f43ae6d2a75d3cc0
Alena Tikhoblazhenko <alanabluecake@gmail.com>
2026-09-25 20:16:08 +0200

    feat: new logging dotenv parameter

--- logging_config.py
+++ logging_config.py
@@ -1,21 +1,35 @@
 import logging 
 from pathlib import Path 
 from datetime import datetime 
+from dotenv import load_dotenv
+import os 
+
 
 def setup_logger():
-    Path("logs").mkdir(exist_ok=True)
+    load_dotenv()
 
-    timestamp = datetime.now().strftime("%H:%M:%S_%d-%m-%Y")
-    log_file = f"logs/{timestamp}.log"
+    if os.getenv("LOGS","true").lower() == "true" or os.getenv("LOGS").lower() not in ["true", "false"]:
+        Path("logs").mkdir(exist_ok=True)
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-        handlers=[
-            logging.FileHandler(log_file, encoding="utf-8"),
-            logging.StreamHandler(),
-        ],
-    )
+        timestamp = datetime.now().strftime("%d%m%Y-%H%M%S")
+        log_file = f"logs/{timestamp}.log"
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+            handlers=[
+                logging.FileHandler(log_file, encoding="utf-8"),
+                logging.StreamHandler(),
+            ],
+        )
+    elif os.getenv("LOGS").lower() == "false":
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        )
+    
+
+       
+
     # logger = logging.getLogger(__name__)
 
     # logger.info("Application started")
